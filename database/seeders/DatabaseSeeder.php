0e44f29b24d7b5b68e90a1ab92720e58344e9b50
Anthony Schuijlenburg <a.schuijlenburg@gmail.com>
2025-01-09 11:44:44 +0100

    initial commit: base laravel project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        if (User::all()->count() === 0) {
+            User::factory()->create([
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+            ]);
+        }
+    }
+}
