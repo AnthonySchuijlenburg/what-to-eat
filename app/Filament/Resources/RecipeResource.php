8edcd0844b29a5853ec5ba1a851cb3f20ed33e8d
Anthony Schuijlenburg <a.schuijlenburg@gmail.com>
2025-01-21 13:31:24 +0100

    feat: add a filament resource for managing recipes/ingredients

---
+++ app/Filament/Resources/RecipeResource.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RecipeResource\Pages;
+use App\Filament\Resources\RecipeResource\RelationManagers\IngredientsRelationManager;
+use App\Models\Recipe;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class RecipeResource extends Resource
+{
+    protected static ?string $model = Recipe::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-cake';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\FileUpload::make('image_url')
+                    ->image()
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->columnSpan(2)
+                    ->maxLength(255),
+                Forms\Components\Toggle::make('variable_size')
+                    ->inline(false)
+                    ->columnSpan(1)
+                    ->required(),
+                Forms\Components\TextInput::make('serves')
+                    ->required()
+                    ->columnSpan(1)
+                    ->numeric(),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\Repeater::make('steps')
+                    ->simple(
+                        TextInput::make('step')->required(),
+                    )
+                    ->required()
+                    ->columnSpanFull(),
+
+            ])
+            ->columns(4);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->limit(50)
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('ingredients_count')
+                    ->counts('ingredients')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('variable_size')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('serves')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\ImageColumn::make('image_url'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->defaultSort('name')
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            IngredientsRelationManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRecipes::route('/'),
+            'create' => Pages\CreateRecipe::route('/create'),
+            'edit' => Pages\EditRecipe::route('/{record}/edit'),
+            'view' => Pages\ViewRecipe::route('/{record}'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/RecipeResource/RelationManagers/IngredientsRelationManager.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Resources\RecipeResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class IngredientsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'ingredients';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('amount'),
+                Tables\Columns\TextColumn::make('amount_in_grams')->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
